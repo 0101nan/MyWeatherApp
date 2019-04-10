6102924dd38b96d0149d9a67e080db41c2dc6164
0101nan <liyinan.heaven@hotmail.com>
2019-04-10 14:44:57 +0800

    修正点击城市列表返回最后一个城市的错误，添加删除城市

--- app/src/main/java/com/liyinan/myweather/AreaAdapter.java
+++ app/src/main/java/com/liyinan/myweather/AreaAdapter.java
@@ -2,11 +2,14 @@ package com.liyinan.myweather;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.liyinan.myweather.db.Area;
 
@@ -38,18 +41,29 @@ public class AreaAdapter extends RecyclerView.Adapter<AreaAdapter.ViewHolder> {
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.area_list_item,parent,false);
         ViewHolder holder=new ViewHolder(view);
+        int position=holder.getAdapterPosition();
         holder.areaView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=WeatherPagerActivity.newIntent(v.getContext(),mArea.getAreaCode());
+                int position=holder.getAdapterPosition();
+                Intent intent=WeatherPagerActivity.newIntent(v.getContext(),mAreaList.get(position).getAreaCode());
                 v.getContext().startActivity(intent);
             }
         });
         holder.areaView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                LitePal.deleteAll(Area.class,"mAreaName=?","mArea.getAreaName()");
-                notifyDataSetChanged();
+                Snackbar.make(v,"是否删除该城市？",Snackbar.LENGTH_SHORT)
+                        .setAction("确定", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                int position=holder.getAdapterPosition();
+                                LitePal.deleteAll(Area.class,"mAreaCode=?",mAreaList.get(position).getAreaCode());
+                                Toast.makeText(v.getContext(), "已删除:"+mAreaList.get(position).getAreaName(), Toast.LENGTH_SHORT).show();
+                                mAreaList=LitePal.findAll(Area.class);
+                                AreaAdapter.this.notifyDataSetChanged();
+                            }
+                        }).show();
                 return true;
             }
         });
